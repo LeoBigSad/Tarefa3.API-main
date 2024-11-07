c19c248cadc25b5f1f780de93f8c967891af5b82
LeoBigSad <85249134+LeoBigSad@users.noreply.github.com>
2024-11-07 14:27:41 -0300

    pasta .API

---
+++ Tarefa3.API/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Emit;
+using Tarefa3.Domain.Interfaces.Service;
+using Tarefa3.Application.Services;
+
+namespace Tarefa3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var people = _personService.GetAll();
+            return Ok(people);
+        }
+    }
+}
